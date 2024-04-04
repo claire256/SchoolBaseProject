a877eb3da30c46cd78ba6eceee9cf4aa87c5187a
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-04-04 17:28:52 +0100

    fix: started the signup page for teachers over

--- src/pages/web-site/teachersOnboading/SignUpPage.jsx
+++ src/pages/web-site/teachersOnboading/SignUpPage.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import '../../../styles/signup-teachers.css'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { signupImg } from "../../../assets";
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const SignUpPage = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  function togglePasswordVisibility() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
   return (
-    <div className="signin-container">
-      <div className="signin-left-container">
-        <Link to={"/"}>
-          <h1>SchoolBase</h1>
+    <div className="flex h-screen bg-gray-100">
+      <div className="hidden lgss:flex lgss:w-1/2 bg-signUpBg flex-col justify-center items-center px-8">
+        <Link to={"/"} className="mb-8">
+          <h1 className="text-4xl font-semibold text-primary">SchoolBase</h1>
         </Link>
-        <img src={signupImg} alt="" />
+        <img src={signupImg} alt="SignUp" className="w-3/4" />
       </div>
-      <form className="signin-right-container">
-        <Link to={"/"}>
-          <h1>Create your account</h1>
-        </Link>
-        <div className="form">
-          <div className="signin-form-row">
-            <div className="signin-form">
-              <label htmlFor="surname" className="label">
+      <form className="w-full lgss:w-1/2 flex flex-col justify-center items-start px-8">
+        <div className="py-4">
+          <h1 className="text-[26px] font-extrabold cursor-pointer">
+            Create your account
+          </h1>
+        </div>
+        <div className="flex flex-col w-full space-y-3">
+          <div className="flex flex-row w-[90%] space-x-8">
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="surname" className="text-[18px] font-semibold">
                 Surname
               </label>
               <input
@@ -27,23 +34,28 @@ const SignUpPage = () => {
                 name="surname"
                 id="surname"
                 placeholder="Surname"
+                className="py-2 shadow-lg shadow-gray-400/70 outline-none px-2"
               />
             </div>
-            <div className="signin-form">
-              <label htmlFor="surname" className="label">
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="surname" className="text-[18px] font-semibold">
                 First Name
               </label>
               <input
                 type="text"
-                name="First Name"
-                id="fname"
+                name="surname"
+                id="surname"
                 placeholder="First Name"
+                className="py-2 outline-none shadow-lg shadow-gray-400/70 px-2"
               />
             </div>
           </div>
-          <div className="signin-form-row">
-            <div className="signin-form">
-              <label htmlFor="middle name" className="label">
+          <div className="flex flex-row w-[90%] space-x-8">
+            <div className="flex flex-col w-1/2">
+              <label
+                htmlFor="middle name"
+                className="text-[18px] font-semibold"
+              >
                 Middle Name
               </label>
               <input
@@ -51,97 +63,141 @@ const SignUpPage = () => {
                 name="surname"
                 id="surname"
                 placeholder="Middle Name"
+                className="py-2 shadow-lg shadow-gray-400/70 outline-none px-2"
               />
             </div>
-            <div className="signin-form">
-              <label htmlFor="surname" className="label">
+            <div className="flex flex-col w-1/2">
+              <label htmlFor="gender" className="text-[18px] font-semibold">
                 Gender
               </label>
-              <input
-                type="text"
-                name="gender"
-                id="gender"
-                placeholder="Gender"
-              />
+              <div className="">
+                <FormControl
+                  className="w-full bg-white text-[#97999C] shadow-lg shadow-gray-400/70"
+                  size="small"
+                >
+                  <Select
+                    label="gender"
+                    sx={{
+                      "& fieldset": { border: "none" },
+                    }}
+                  >
+                    <MenuItem value="Male">Male</MenuItem>
+                    <MenuItem value="Female">Female</MenuItem>
+                    <MenuItem value="Other">Other</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
             </div>
           </div>
-          <div className="signin-form-row">
-            <div className="signin-form">
-              <label htmlFor="Residential address" className="label">
+          <div className="flex flex-row w-[90%] space-x-8">
+            <div className="flex flex-col w-1/2">
+              <label
+                htmlFor="residential address"
+                className="text-[18px] font-semibold"
+              >
                 Residential Address
               </label>
               <input
                 type="text"
-                name="Residential Address"
-                id="residentialAddress"
+                name="residential address"
+                id="residence"
                 placeholder="Residential Address"
+                className="py-2 shadow-lg shadow-gray-400/70 outline-none px-2"
               />
             </div>
-            <div className="signin-form">
-              <label htmlFor="email address" className="label">
+            <div className="flex flex-col w-1/2">
+              <label
+                htmlFor="Email Address"
+                className="text-[18px] font-semibold"
+              >
                 Email Address
               </label>
               <input
                 type="text"
-                name="email address"
-                id="emailAddress"
-                placeholder="Email Address "
+                name="Email Address"
+                id="email"
+                placeholder="Email Address"
+                className="py-2 outline-none shadow-lg shadow-gray-400/70 px-2"
               />
             </div>
           </div>
-          <div className="signin-form-row">
-            <div className="signin-form">
-              <label htmlFor="class" className="label">
-                Class
+          <div className="flex flex-row w-[90%] space-x-8">
+            <div className="flex flex-col w-1/2">
+              <label
+                htmlFor="residential address"
+                className="text-[18px] font-semibold"
+              >
+                Password
               </label>
-              <input type="text" name="class" id="class" placeholder="Class" />
+              <div className="  bg-white flex outline-none text-[16px] w-full justify-between items-center py-2 shadow-lg shadow-gray-400/70 px-2">
+                <input
+                  type={isPasswordVisible ? "text" : "password"}
+                  placeholder="Password"
+                  className="outline-none w-full"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+
+                {isPasswordVisible ? (
+                  <IoMdEye
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative"
+                  />
+                ) : (
+                  <IoMdEyeOff
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative "
+                  />
+                )}
+              </div>
             </div>
-            <div className="signin-form">
-              <label htmlFor="Phone Number" className="label">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="Phone Number"
-                id="phoneNumber"
-                placeholder="Phone Number"
-              />
-            </div>
-          </div>
-          <div className="signin-form-row">
-            <div className="signin-form">
-              <label htmlFor="newPassword" className="label">
-                New Password
-              </label>
-              <input
-                type="text"
-                name="newPassword"
-                id="newPassword"
-                placeholder="New Password"
-              />
-            </div>
-            <div className="signin-form">
-              <label htmlFor="confirm password" className="label">
+            <div className="flex flex-col w-1/2">
+              <label
+                htmlFor="residential address"
+                className="text-[18px] font-semibold"
+              >
                 Confirm Password
               </label>
-              <input
-                type="text"
-                name="confirm password"
-                id="confirmPassword"
-                placeholder="Confirm Password"
-              />
+              <div className="  bg-white flex outline-none text-[16px] w-full justify-between items-center py-2 shadow-lg shadow-gray-400/70 px-2">
+                <input
+                  type={isPasswordVisible ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  className="outline-none w-full"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+
+                {isPasswordVisible ? (
+                  <IoMdEye
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative"
+                  />
+                ) : (
+                  <IoMdEyeOff
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative "
+                  />
+                )}
+              </div>
             </div>
           </div>
-        <Link to={"/teacher/dashboard"}>
-        <button className="signup-btn">Sign Up</button>
+        </div>
+        <Link className="w-[90%]" to={"/teacher/dashboard"}>
+          <button className="bg-primary w-full py-3 mt-8 text-white font-semibold text-[18px] rounded-[16px]">
+            Sign Up
+          </button>
         </Link>
-        <p className="not-registered">
+        <p className="text-center w-full font-semibold mt-3">
           Already have an account?{" "}
-          <Link to={"/get-started/teacher/signin"}>
-            <span>Login</span>
+          <Link
+            to={"/get-started/teacher/signin"}
+            className="text-red-300 underline px-2 font-medium"
+          >
+            Sign In
           </Link>
         </p>
-        </div>
       </form>
     </div>
   );
