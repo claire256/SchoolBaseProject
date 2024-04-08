3c2aa748d0d6a80eff850a91be4b071a90902f04
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-04-08 19:41:43 +0100

    fix: hot fix

--- src/pages/web-site/teachersOnboading/SignUpPage.jsx
+++ src/pages/web-site/teachersOnboading/SignUpPage.jsx
@@ -11,8 +11,8 @@ const SignUpPage = () => {
     setIsPasswordVisible((prevState) => !prevState);
   }
   return (
-    <div className="flex bg-gray-100">
-      <div className="bg-signUpBg lgss:w-1/2 flex flex-col justify-start items-start relative h-screen">
+    <div className="flex h-screen bg-gray-100">
+      <div className="bg-signUpBg lgss:w-1/2 flex flex-col justify-start items-start relative h-full">
         <Link to={"/"} className="px-5 py-3">
           <h1 className="text-3xl sm:text-4xl font-semibold text-primary font-itim">
             SchoolBase
