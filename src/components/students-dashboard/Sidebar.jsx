c8e05d0d7d2328351af88476cee13f2cd46fb030
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-04-08 18:43:47 +0100

    fix: hot fix

--- src/components/students-dashboard/Sidebar.jsx
+++ src/components/students-dashboard/Sidebar.jsx
@@ -34,8 +34,90 @@ const Sidebar = ({ isOpen }) => {
   }, [location.pathname]);
   return (
     <>
-      <div className="border w-[20%] hidden lgss:flex flex-col items-center pt-12 pb-10 justify-between font-bold overflow-y-hidden text-[#7D7676] text-sm">
+      <div className="border w-[20%] hidden lgss:flex flex-col items-center pt-12 pb-10 justify-between font-bold overflow-y-hidden text-[#7D7676] text-sm sidebar">
+        <div className="w-full h-[60px] text-[26px] flex flex-col gap-4 font-extrabold">
+          <img src={schoolbaseLogo} alt="" className="w-full h-full bg-cover" />
+          <h1 className="text-center">
+            SCHOOL <span className="text-primary">BASE</span>
+          </h1>
+        </div>
+        <div className="mt-16 w-full flex flex-col gap-2 justify-start items-center font-manrope">
+          <Link
+            to="/student/dashboard"
+            className={
+              activebutton === 1
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <FaHome />
+            <h4 className=" text-[18px]">Home</h4>
+          </Link>
+          <Link
+            to="/student/student-life"
+            className={
+              activebutton === 2
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <FaGraduationCap />
+            <h4 className=" text-[18px]">Student life</h4>
+          </Link>
+          <Link
+            to="/student/classes"
+            className={
+              activebutton === 3
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <BsPeopleFill />
+            <h4 className="text-[18px]">Classes</h4>
+          </Link>
+          <Link
+            to="/student/payments"
+            className={
+              activebutton === 4
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <FaUserPen />
+            <h4 className="">Payments</h4>
+          </Link>
+          <Link
+            to="/student/mail"
+            className={
+              activebutton === 5
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <BsFillCreditCardFill />
 
+            <h4 className="">Mail</h4>
+          </Link>
+          <Link
+            to="/student/settings"
+            className={
+              activebutton === 6
+                ? "flex text-[18px] text-white items-center font-medium pl-16 bg-primary w-full gap-5 h-[60px]"
+                : "flex items-center text-[18px] text-[#6B7276] font-medium pl-16 w-full gap-5 h-[60px]"
+            }
+          >
+            <RiBookOpenFill />
+            <h4 className="">Settings</h4>
+          </Link>
+
+          <button
+            onClick={() => navigate("/get-started")}
+            className="flex gap-5 text-[18px] mt-4 text-[#B82323] items-center  font-medium pl-16 h-[40%] w-full border-t-2 "
+          >
+            Sign Out
+            <HiOutlineLogout />
+          </button>
+        </div>
       </div>
 
       {isOpen && (
