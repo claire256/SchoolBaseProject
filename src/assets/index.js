e75835eac464a07a875c962882e5bf82e9634faa
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-04-13 11:04:55 +0100

    feat: merged branches

--- src/assets/index.js
+++ src/assets/index.js
@@ -59,8 +59,6 @@ import dotts from "../assets/images/dotts.png";
 import colleaguesDiscussing from "../assets/images/colleaguesdiscussing.svg";
 import Arrowdown from "../assets/images/Arrowdown.svg";
 import imageicon from "../assets/images/imageicon.svg";
-import Notification from "../assets/images/Notification.png";
-import calender from "../assets/images/calender.png";
 import vector from "../assets/images/TeacherAttendance/Vector.svg";
 import man from "../assets/images/TeacherAttendance/man.svg";
 import chevleft from "../assets/images/TeacherAttendance/chevleft.svg";
@@ -142,8 +140,6 @@ export {
   Arrowdown,
   imageicon,
   Manlearning,
-  Notification,
-  bell,
   vector,
   man,
   chevleft,
