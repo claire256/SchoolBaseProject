65b79f9a05c9c3447e727115d8f779bedb9c5e82
Cynthia Amaji <cynthyok4luv@gmail.com>
2024-04-17 10:27:32 +0100

    teachers dashboard settings

--- src/components/teachers-dashboard/Settings.jsx
+++ src/components/teachers-dashboard/Settings.jsx
@@ -167,27 +167,3 @@ const Settings = () => {
 };
 
 export default Settings;
-
-{/*<div className="flex">
-      <Sidebar isOpen={isOpen} />
-      <div className="lgss:w-4/5 w-full lgss:overflow-auto">
-        <div className="lgss:hidden flex justify-end w-full pt-5">
-          {isOpen ? (
-            <FaTimes
-              onClick={() => setIsOpen(false)}
-              className=" cursor-pointer text-secondary text-xl"
-            />
-          ) : (
-            <FaBars
-              onClick={() => setIsOpen(true)}
-              className=" cursor-pointer text-secondary text-xl"
-            />
-          )}
-        </div>
-        <Dashboard />
-      </div>
-    </div>
-  );
-};
-
-export default Settings;*/}
