38cf8d62f5c17b1d0a8f455316c81ea06e7905ae
Cynthia Amaji <cynthyok4luv@gmail.com>
2024-04-24 01:15:57 +0100

    modal update

--- src/components/teachers-dashboard/Classes/Subject.jsx
+++ src/components/teachers-dashboard/Classes/Subject.jsx
@@ -1,14 +1,14 @@
 import { book } from "../../../assets";
 import "../../../styles/teacherdashboard/Subject.css";
 
-const Subject = ()=>{
-    return(
-        <div className="sub-card">
-          <img src={book} className="sub-image"/>
-          <div className="btn-display">
-              <p>Mathematics</p>
-              <button className="blue-btn">View Content</button>
-          </div>
+const Subject = ({ subjectname }) => {
+    return (
+        <div className="sub-card ml-12 mr-12 mb-10">
+            <img src={book} className="sub-image" />
+            <div className="btn-display">
+                <p>{subjectname}</p>
+                <button className="blue-btn">View Content</button>
+            </div>
         </div>
     )
 }
